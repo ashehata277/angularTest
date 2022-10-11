fa4ec505a52eea1a61796d5753da06c74cc18fa3
ahmed.shehata <ahmed.shehata@hadafsolutions.com>
2022-10-11 23:18:45 +0200

    -

--- src/app/Services/RouterGaurds/AuthorizationGuard.ts
+++ src/app/Services/RouterGaurds/AuthorizationGuard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
-import { from, Observable } from "rxjs";
+import {  Observable } from "rxjs";
 import { OAuthService } from "../AuthService/OAuth2service";
 
 @Injectable({ providedIn: 'root' })
