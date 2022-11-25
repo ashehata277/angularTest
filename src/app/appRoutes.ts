9c62fd2a5158e63e130d42acb38820e4a46bdfa0
ahmed.shehata <ahmed.shehata@hadafsolutions.com>
2022-11-26 00:09:46 +0200

    -

--- src/app/appRoutes.ts
+++ src/app/appRoutes.ts
@@ -8,28 +8,23 @@ import { SignRedirectComponentComponent } from "./SignComponents/sign-redirect-c
 
 
 const appRoutes: Routes = [
-    { path: 'signin-callback', component: SignRedirectComponentComponent },
-    { path: 'signout-callback', component: SignOutRedirectComponentComponent },
-    { path: 'AccessDenied', component: NotAuthorizedComponentComponent },
-    {
-        path: 'test-module',
-        loadChildren: () => import('./Modules/test-module/test-module.module').then(m => m.TestModuleModule),
-        canActivate: [AuthorizationGuard],
-        data: { ModuleId: 201 }
-    },
-    {
-      path: 'test-module-two',
-      loadChildren: () => import('./Modules/test-module-two/test-module-two.module').then(m => m.TestModuleTwoModule),
-      canActivate: [AuthorizationGuard],
-      data: { ModuleId: 201 }
+  { path: 'signin-callback', component: SignRedirectComponentComponent },
+  { path: 'signout-callback', component: SignOutRedirectComponentComponent },
+  { path: 'AccessDenied', component: NotAuthorizedComponentComponent },
+  {
+    path: 'test-module',
+    loadChildren: () => import('./Modules/test-module/test-module.module').then(m => m.TestModuleModule),
+    canActivate: [AuthorizationGuard],
+    data: { ModuleId: 201 }
   }
+
 ]
 
 
 @NgModule({
-  imports:[RouterModule.forRoot(appRoutes)],
-  exports:[RouterModule]
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule{
+export class AppRoutingModule {
 
 }
